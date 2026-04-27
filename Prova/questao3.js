5cfe08976e304073d825e850682d15ce91bfec7b
di0x1 <ricardogamer194@gmail.com>
2026-04-27 08:26:07 -0300

    Prova

---
+++ Prova/questao3.js
@@ -0,0 +1,10 @@
+function mostrarTabela() {
+    var imagem = document.createElement("img");
+    imagem.src = "img/Tabela_Jogos.png";
+    imagem.alt = "Tabela Oficial de Jogos - Copa do Mundo FIFA 2026";
+    imagem.style.width = "100%";
+    imagem.style.borderRadius = "10px";
+    imagem.style.marginTop = "20px";
+
+    document.body.appendChild(imagem);
+}
